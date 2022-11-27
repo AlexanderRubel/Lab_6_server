f7dbf42203f05efdeb9e10e72f12785cb9f24f36
Alexander <rubel.sasha.2002@yandex.ru>
2022-11-27 10:19:17 +0300

    Initial

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <vector>
 #include <initguid.h>
+#include <strsafe.h>
 
 #include "Device.h"
 
@@ -29,6 +30,7 @@ DEFINE_GUID(g_guidServiceClass, 0xb62c4e8d, 0x62cc, 0x404b, 0xbb, 0xbf, 0xbf, 0x
 wchar_t g_szRemoteName[BTH_MAX_NAME_SIZE + 1] = { 0 };  // 1 extra for trailing NULL character
 wchar_t g_szRemoteAddr[CXN_BDADDR_STR_LEN + 1] = { 0 }; // 1 extra for trailing NULL character
 int  g_ulMaxCxnCycles = 1;
+#define CXN_INSTANCE_STRING L"Sample Bluetooth Server"
 
 int __cdecl main()
 {
@@ -76,67 +78,116 @@ int __cdecl main()
     HANDLE hLookup{};
     DWORD  flags = LUP_CONTAINERS | LUP_FLUSHCACHE | LUP_RETURN_NAME | LUP_RETURN_ADDR;
 
-    if (WSALookupServiceBegin(&wsaQuery, flags, &hLookup) == SOCKET_ERROR) {
-        wprintf(L"WSALookupServiceBegin failed with error: %d\n", WSAGetLastError());
-        return 1;
-    }
-
-    std::vector<Device> devices;
-    int i = 0;
-    devices.emplace_back();
     
-    while (devices[i].getDeviceInfo(hLookup, flags) == NO_ERROR) {
-        devices[i].printDeviceInfo();
-        devices.emplace_back();
-
-        Sleep(1000);
-        wprintf(L"\n");
-        i++;
-    }
-    if (WSAGetLastError() != WSA_E_NO_MORE) {
-        devices.pop_back();
-    }
-    WSALookupServiceEnd(hLookup);
-
-    int devNum = 0;
-    wprintf(L"______________________\n");
-    wprintf(L"Choose device:\n>");
-    std::cin >> devNum;
 
     /* Run client mode */
     /* Creating SOCKET */
-    devices[devNum].pBtSockRemote.addressFamily = AF_BTH;
-    devices[devNum].pBtSockRemote.serviceClassId = g_guidServiceClass;
-    devices[devNum].pBtSockRemote.port = 0;
+    LPCSADDR_INFO   lpCSAddrInfo = NULL;
+    lpCSAddrInfo = (LPCSADDR_INFO)HeapAlloc(GetProcessHeap(),
+        HEAP_ZERO_MEMORY,
+        sizeof(CSADDR_INFO));
+    if (NULL == lpCSAddrInfo) {
+        wprintf(L"!ERROR! | Unable to allocate memory for CSADDR_INFO\n");
+    }
 
-    SOCKET localSocket = socket(AF_BTH, SOCK_STREAM, BTHPROTO_RFCOMM);
-    if (INVALID_SOCKET == localSocket) {
+    wchar_t         szThisComputerName[MAX_COMPUTERNAME_LENGTH + 1];
+    DWORD           dwLenComputerName = MAX_COMPUTERNAME_LENGTH + 1;
+    if (!GetComputerName(szThisComputerName, &dwLenComputerName)) {
+        wprintf(L"=CRITICAL= | GetComputerName() call failed. WSAGetLastError=[%d]\n", WSAGetLastError());
+    }
+
+    SOCKET LocalSocket = socket(AF_BTH, SOCK_STREAM, BTHPROTO_RFCOMM);
+    if (INVALID_SOCKET == LocalSocket) {
         wprintf(L"=CRITICAL= | socket() call failed. WSAGetLastError = [%d]\n", WSAGetLastError());
     }
 
-    //
-    // Connect the socket (pSocket) to a given remote socket represented by address (pServerAddr)
-    //
-    if (SOCKET_ERROR == connect(localSocket,
-        (struct sockaddr*)&devices[devNum].pBtSockRemote,
+    SOCKADDR_BTH    SockAddrBthLocal = { 0 };
+    SockAddrBthLocal.addressFamily = AF_BTH;
+    SockAddrBthLocal.port = BT_PORT_ANY;
+
+    if (SOCKET_ERROR == bind(LocalSocket,
+        (struct sockaddr*)&SockAddrBthLocal,
         sizeof(SOCKADDR_BTH))) {
-        wprintf(L"=CRITICAL= | connect() call failed. WSAGetLastError=[%d]\n", WSAGetLastError());
+        wprintf(L"=CRITICAL= | bind() call failed w/socket = [0x%I64X]. WSAGetLastError=[%d]\n", (ULONG64)LocalSocket, WSAGetLastError());
     }
 
-    char buf[7] = "Hello!";
-    std::cout << "___________________________" << std::endl;
-    if (SOCKET_ERROR == send(localSocket, (char*)buf, (int)7, 0)) {
-        wprintf(L"=CRITICAL= | send() call failed WSAGetLastError=[%d]\n", WSAGetLastError());
+    int iAddrLen = sizeof(SOCKADDR_BTH);
+    int ulRetCode = getsockname(LocalSocket,
+        (struct sockaddr*)&SockAddrBthLocal,
+        &iAddrLen);
+    if (SOCKET_ERROR == ulRetCode) {
+        wprintf(L"=CRITICAL= | getsockname() call failed w/socket = [0x%I64X]. WSAGetLastError=[%d]\n", (ULONG64)LocalSocket, WSAGetLastError());
+        ulRetCode = CXN_ERROR;
     }
+
+    lpCSAddrInfo[0].LocalAddr.iSockaddrLength = sizeof(SOCKADDR_BTH);
+    lpCSAddrInfo[0].LocalAddr.lpSockaddr = (LPSOCKADDR)&SockAddrBthLocal;
+    lpCSAddrInfo[0].RemoteAddr.iSockaddrLength = sizeof(SOCKADDR_BTH);
+    lpCSAddrInfo[0].RemoteAddr.lpSockaddr = (LPSOCKADDR)&SockAddrBthLocal;
+    lpCSAddrInfo[0].iSocketType = SOCK_STREAM;
+    lpCSAddrInfo[0].iProtocol = BTHPROTO_RFCOMM;
+
+    WSAQUERYSET     wsaQuerySet = { 0 };
+    ZeroMemory(&wsaQuerySet, sizeof(WSAQUERYSET));
+    wsaQuerySet.dwSize = sizeof(WSAQUERYSET);
+    wsaQuerySet.lpServiceClassId = (LPGUID)&g_guidServiceClass;
+
+    HRESULT         res;
+    size_t          cbInstanceNameSize = 0;
+    res = StringCchLength(szThisComputerName, sizeof(szThisComputerName), &cbInstanceNameSize);
+    if (FAILED(res)) {
+        wprintf(L"-FATAL- | ComputerName specified is too large\n");
+        ulRetCode = CXN_ERROR;
+    }
+
+    wchar_t* pszInstanceName = NULL;
+    cbInstanceNameSize += sizeof(CXN_INSTANCE_STRING) + 1;
+    pszInstanceName = (LPWSTR)HeapAlloc(GetProcessHeap(),
+        HEAP_ZERO_MEMORY,
+        cbInstanceNameSize);
+    if (NULL == pszInstanceName) {
+        wprintf(L"-FATAL- | HeapAlloc failed | out of memory | gle = [%d] \n", GetLastError());
+        ulRetCode = CXN_ERROR;
+    }
+
+    wchar_t comment[] = L"Example Service instance registered in the directory service through RnR";
+    StringCbPrintf(pszInstanceName, cbInstanceNameSize, L"%s %s", szThisComputerName, CXN_INSTANCE_STRING);
+    wsaQuerySet.lpszServiceInstanceName = pszInstanceName;
+    wsaQuerySet.lpszComment = comment;
+    wsaQuerySet.dwNameSpace = NS_BTH;
+    wsaQuerySet.dwNumberOfCsAddrs = 1;      // Must be 1.
+    wsaQuerySet.lpcsaBuffer = lpCSAddrInfo; // Req'd.
 
     //
-    // Close the socket
+    // As long as we use a blocking accept(), we will have a race
+    // between advertising the service and actually being ready to
+    // accept connections.  If we use non-blocking accept, advertise
+    // the service after accept has been called.
     //
-    if (SOCKET_ERROR == closesocket(localSocket)) {
-        wprintf(L"=CRITICAL= | closesocket() call failed WSAGetLastError=[%d]\n", WSAGetLastError());
+    if (SOCKET_ERROR == WSASetService(&wsaQuerySet, RNRSERVICE_REGISTER, 0)) {
+        wprintf(L"=CRITICAL= | WSASetService() call failed. WSAGetLastError=[%d]\n", WSAGetLastError());
+        ulRetCode = CXN_ERROR;
     }
 
+    if (SOCKET_ERROR == listen(LocalSocket, CXN_DEFAULT_LISTEN_BACKLOG)) {
+        wprintf(L"=CRITICAL= | listen() call failed w/socket = [0x%I64X]. WSAGetLastError=[%d]\n", (ULONG64)LocalSocket, WSAGetLastError());
+        ulRetCode = CXN_ERROR;
+    }
+
+    SOCKET ClientSocket = INVALID_SOCKET;
+    ClientSocket = accept(LocalSocket, NULL, NULL);
     /* User code ends here */
+    char buff[7] = {};
+    int iLengthReceived = recv(ClientSocket,
+        (char*)buff, 7, 0);
+
+    if (closesocket(ClientSocket)) {
+        wprintf(L"=CRITICAL= | closesocket() call failed w/socket = [0x%I64X]. WSAGetLastError=[%d]\n", (ULONG64)ClientSocket, WSAGetLastError());
+    }
+    if (closesocket(LocalSocket)) {
+        wprintf(L"=CRITICAL= | closesocket() call failed w/socket = [0x%I64X]. WSAGetLastError=[%d]\n", (ULONG64)LocalSocket, WSAGetLastError());
+    }
+
 
     WSACleanup();
 
